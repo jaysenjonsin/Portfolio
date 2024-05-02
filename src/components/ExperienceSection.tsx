3780a8e20f58e7c665b41bc399256ab6a43ef51a
jaysenjonsin <jasoncjohnson5@yahoo.com>
2024-05-02 16:32:13 -0700

    experience styling

--- src/components/ExperienceSection.tsx
+++ src/components/ExperienceSection.tsx
@@ -1,10 +1,11 @@
+'use client';
 import React from 'react';
 
 type Props = {};
 
 const ExperienceSection = (props: Props) => {
   return (
-    <section id='experince'>
+    <section id='experience'>
       <h1 className='title'>Experience</h1>
       <div className='experience-details-container'>
         <div className='about-containers'>
@@ -22,10 +23,122 @@ const ExperienceSection = (props: Props) => {
                   <p>Experienced</p>
                 </div>
               </article>
+              <article>
+                <img
+                  src='/checkmark.png'
+                  alt='experience icon'
+                  className='icon'
+                />
+                <div>
+                  <h3>HTML</h3>
+                  <p>Experienced</p>
+                </div>
+              </article>
+              <article>
+                <img
+                  src='/checkmark.png'
+                  alt='experience icon'
+                  className='icon'
+                />
+                <div>
+                  <h3>HTML</h3>
+                  <p>Experienced</p>
+                </div>
+              </article>
+              <article>
+                <img
+                  src='/checkmark.png'
+                  alt='experience icon'
+                  className='icon'
+                />
+                <div>
+                  <h3>CSS</h3>
+                  <p>Experienced</p>
+                </div>
+              </article>
+              <article>
+                <img
+                  src='/checkmark.png'
+                  alt='experience icon'
+                  className='icon'
+                />
+                <div>
+                  <h3>HTML</h3>
+                  <p>Experienced</p>
+                </div>
+              </article>
+            </div>
+          </div>
+          <div className='detail-containers'>
+            <h2 className='experience-sub-title'>Backend Development</h2>
+            <div className='article-container'>
+              <article>
+                <img
+                  src='/checkmark.png'
+                  alt='experience icon'
+                  className='icon'
+                />
+                <div>
+                  <h3>HTML</h3>
+                  <p>Experienced</p>
+                </div>
+              </article>
+              <article>
+                <img
+                  src='/checkmark.png'
+                  alt='experience icon'
+                  className='icon'
+                />
+                <div>
+                  <h3>HTML</h3>
+                  <p>Experienced</p>
+                </div>
+              </article>
+              <article>
+                <img
+                  src='/checkmark.png'
+                  alt='experience icon'
+                  className='icon'
+                />
+                <div>
+                  <h3>HTML</h3>
+                  <p>Experienced</p>
+                </div>
+              </article>
+              <article>
+                <img
+                  src='/checkmark.png'
+                  alt='experience icon'
+                  className='icon'
+                />
+                <div>
+                  <h3>CSS</h3>
+                  <p>Experienced</p>
+                </div>
+              </article>
+              <article>
+                <img
+                  src='/checkmark.png'
+                  alt='experience icon'
+                  className='icon'
+                />
+                <div>
+                  <h3>HTML</h3>
+                  <p>Experienced</p>
+                </div>
+              </article>
             </div>
           </div>
         </div>
       </div>
+      <img
+        src='/arrow.png'
+        alt='Arrow icon'
+        className='icon arrow'
+        onClick={() => {
+          location.href = './#projects';
+        }}
+      />
     </section>
   );
 };
