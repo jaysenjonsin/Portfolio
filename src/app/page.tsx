8a4378e15bf528bb6a809e121ff363772aa84aea
jaysenjonsin <jasoncjohnson5@yahoo.com>
2024-05-02 16:00:10 -0700

    about section styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from 'next/image';
 import styles from './page.module.css';
 import Header from '../components/Header';
 import ProfileSection from '../components/ProfileSection';
+import AboutSection from '../components/AboutSection';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Header />
       <ProfileSection />
+      <AboutSection />
     </main>
   );
 }
